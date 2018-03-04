8a19aa4749ef3712c7d4c1b9a5abf4eef967972a
Kristian-A <kristian.zh.apostolov@gmail.com>
2018-03-04 15:25:06 +0200

    Add files via upload

---
+++ Assets/movement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float speed;
+	void FixedUpdate () {
+        float xDir = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(xDir*speed, 0, 0);
+	}
+}
---
+++ Assets/parallax/parallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour {
+    public float distance;
+    bool central = false;
+    float offset;
+    void Start() {
+        offset = transform.position.x;
+    }
+
+    void FixedUpdate() {
+        Vector3 pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = new Vector3(offset + (-pos.x / distance), transform.position.y, transform.position.z);
+    }
+
+    public void setCentral() {
+        central = true;
+    }
+
+    public void printCentral() {
+        Debug.Log(central);
+    }
+}
